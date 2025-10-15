096e92e1bfaf60a39e4fd18e414b22cda50ee010
Enrico Weigelt, metux IT consult <info@metux.net>
2025-10-15 13:06:48 +0200

    1

--- build.sh
+++ build.sh
@@ -6,9 +6,12 @@ ROOT=`pwd`
 
 export GCLIENT_SHALLOW=1
 
+mkdir -p ./code/chromium_git/chromium/src/out/Debug_GN_x86
+
 python3 \
     $ROOT/automate-git.py \
     --download-dir=$ROOT/code/chromium_git \
     --depot-tools-dir=$ROOT/code/depot_tools \
     --no-distrib \
     --force-build
+
