f8c33b788a0180ba00cc6d67e0fc6d39e71c1d02
Enrico Weigelt, metux IT consult <info@metux.net>
2025-12-03 12:35:09 +0100

    add binary tarball upload to github
    upload-release.sh uploads the previous built binary distribution
    tarball as release artifact on the `metux/cef-chromium` github project.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

---
+++ upload-release.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+BINARY_DIR=./code/chromium_git/chromium/src/cef/binary_distrib
+DOWNLOAD_DIR=tmp
+
+RELEASE="2025-12-03-02"
+
+die() {
+    echo "ERR: $*" >&2
+    exit 1
+}
+
+FN=`echo $BINARY_DIR/cef_binary_*_linux64.tar.bz2`
+BN=`basename "$FN"`
+
+echo "Using CEF tarball: $FN"
+
+[ -f "$FN" ] || die "cant find tarball file: $FN"
+
+UPLOAD_FILE=cef_binary-$RELEASE.tar.bz2
+
+rm -Rf tmp
+mkdir -p tmp
+
+cp $FN tmp/$UPLOAD_FILE
+
+gh release create $RELEASE tmp/$UPLOAD_FILE \
+    --repo metux/cef-chromium \
+    --title "prebuilt CEF binary: $RELEASE" \
+    --notes "built locally"
