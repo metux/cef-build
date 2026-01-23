ab2e095b1500bf8198656ab232841d3e9854ed1e
Enrico Weigelt, metux IT consult <info@metux.net>
2026-01-23 16:45:23 +0100

    conf.sh: use suckless-145.0.7632.0 CEF branch
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- conf.sh
+++ conf.sh
@@ -8,13 +8,12 @@ DEPOT_TOOLS_DIR=$CODE_DIR/depot_tools
 DEPOT_TOOLS_REPO="https://chromium.googlesource.com/chromium/tools/depot_tools.git"
 DEPOT_TOOLS_BRANCH="main"
 
-#CEF_REPO="https://bitbucket.org/chromiumembedded/cef.git"
 CEF_REPO="https://github.com/metux/cef-suckless"
-CEF_BRANCH="master"
+CEF_BRANCH="suckless-145.0.7632.0"
 
 CHROMIUM_UPSTREAM_REPO="https://chromium.googlesource.com/chromium/src.git"
-CHROMIUM_UPSTREAM_TAG="142.0.7444.0"
-CHROMIUM_BRANCH="cef_142.0.7444.0"
+CHROMIUM_UPSTREAM_TAG="145.0.7632.0"
+CHROMIUM_BRANCH="cef_145.0.7632.0"
 CHROMIUM_WORKDIR="$ROOT/code/chromium_git/chromium/src"
 
 # used by our upload-release.sh script
